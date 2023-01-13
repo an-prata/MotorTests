62f347b6e4e8727be72a08ac4d78ca181a4427b4
evan <evanrileyoverman@gmail.com>
2023-01-13 01:59:12 -0800

    Move motor test to its own class

---
+++ src/main/java/frc/robot/GenericMotorTest.java
@@ -0,0 +1,45 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class GenericMotorTest {
+    private static final boolean IS_TALON_MOTOR = false;
+    private static final int CONTROLLER_PORT = 0;
+    private static final int MOTOR_CAN_ID = 2;
+    
+    private XboxController xboxController;
+    private TalonSRX talon;
+    private CANSparkMax canSparkMax;
+
+    public void init() {
+        // init motors based on MOTOR_CAN_ID constant
+        if (IS_TALON_MOTOR)
+            talon = new TalonSRX(MOTOR_CAN_ID);
+        else
+            canSparkMax = new CANSparkMax(MOTOR_CAN_ID, MotorType.kBrushless);
+
+        // init controller (man, having a motor controller AND xbox controller
+        // is hella confusing).
+        xboxController = new XboxController(CONTROLLER_PORT);
+    }
+
+    public void periodic() {
+        double setSpeed = xboxController.getLeftY();
+  
+        if (IS_TALON_MOTOR) {
+            talon.set(TalonSRXControlMode.PercentOutput, setSpeed);
+        } else {
+            canSparkMax.set(setSpeed);
+            RelativeEncoder encoder = canSparkMax.getEncoder();
+            SmartDashboard.putNumber("CANSparkMax Encoder Position", encoder.getPosition());
+        }
+    }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,13 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.RelativeEncoder;
-import edu.wpi.first.wpilibj.XboxController;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -20,97 +13,69 @@ import edu.wpi.first.wpilibj.XboxController;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final boolean IS_TALON_MOTOR = false;
-  private static final int CONTROLLER_PORT = 0;
-  private static final int MOTOR_CAN_ID = 2;
-  
-  private XboxController xboxController;
-  private TalonSRX talon;
-  private CANSparkMax canSparkMax;
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {}
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // init motors based on MOTOR_CAN_ID constant
-    if (IS_TALON_MOTOR)
-      talon = new TalonSRX(MOTOR_CAN_ID);
-    else
-      canSparkMax = new CANSparkMax(MOTOR_CAN_ID, MotorType.kBrushless);
-  
-    // init controller (man, having a motor controller AND xbox controller
-    // is hella confusing).
-    xboxController = new XboxController(CONTROLLER_PORT);
-  }
+    /**
+     * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+     * that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {}
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {}
+    /**
+     * This autonomous (along with the chooser code above) shows how to select between different
+     * autonomous modes using the dashboard. The sendable chooser code works with the Java
+     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+     * uncomment the getString line to get the auto name from the text box below the Gyro
+     *
+     * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+     * below with additional strings. If using the SendableChooser make sure to add them to the
+     * chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {}
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {}
+    
+    /** This function is called once when teleop is enabled. */
+    @Override
+    public void teleopInit() {}
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {}
-  
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {}
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-    double setSpeed = xboxController.getLeftY();
-  
-    if (IS_TALON_MOTOR) {
-      talon.set(TalonSRXControlMode.PercentOutput, setSpeed);
-    } else {
-      canSparkMax.set(setSpeed);
-      RelativeEncoder encoder = canSparkMax.getEncoder();
-      SmartDashboard.putNumber("CANSparkMax Encoder Position", encoder.getPosition());
-    }
-  }
+    /** This function is called once when the robot is disabled. */
+    @Override
+    public void disabledInit() {}
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+    /** This function is called periodically when disabled. */
+    @Override
+    public void disabledPeriodic() {}
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+    /** This function is called once when test mode is enabled. */
+    @Override
+    public void testInit() {}
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {}
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /** This function is called once when the robot is first started up. */
+    @Override
+    public void simulationInit() {}
 
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {}
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {}
+    /** This function is called periodically whilst in simulation. */
+    @Override
+    public void simulationPeriodic() {}
 }
